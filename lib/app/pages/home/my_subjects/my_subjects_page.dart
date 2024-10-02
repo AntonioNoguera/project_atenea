dc2e6b889d9fb3e615d9b0e64467b9f34400b5db
Michael Guzmán <116075308+AntonioNoguera@users.noreply.github.com>
2024-10-02 17:32:04 -0600

    Revert "Feat [ Default Styles 4 Subject Component ] -> Created more faithful to the design"
    This reverts commit 971cd2b5c8e11641c8974f19b80b347c4e8b48b6.

--- lib/app/pages/home/my_subjects/my_subjects_page.dart
+++ lib/app/pages/home/my_subjects/my_subjects_page.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:proyect_atenea/app/pages/home/home_widgets/home_subject.dart'; 
+import 'package:flutter/material.dart'; 
  
 import 'package:proyect_atenea/app/widgets/AteneaScaffold.dart';
+  
 
 import 'package:proyect_atenea/app/values/AppTheme.dart';  
+  
 import 'package:proyect_atenea/app/widgets/AteneaButton.dart';  
 
 class MySubjectsPage extends StatelessWidget {
@@ -15,26 +16,23 @@ class MySubjectsPage extends StatelessWidget {
     return AteneaScaffold( 
       body : Padding(
         padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.05,
           vertical: 50.0
           ),
         
         child: Column( 
           children: [
             Text(
-                'Mis Materias',
-                style: AppTextStyles.builder(
-                      color: AppColors.primaryColor, 
-                      size: FontSizes.h2, 
-                      weight: FontWeights.regular
-                    )
-              ), 
-           
+              'Mis Materias',
+              style: AppTextStyles.builder(
+                    color: AppColors.primaryColor, 
+                    size: FontSizes.h2, 
+                    weight: FontWeights.regular
+                  )
+            ),
 
             const SizedBox(height: 20.0,),
-
-            Padding(padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.05,),
-              child: AteneaButton(
+             AteneaButton(
               text : 'Añadir Nueva Materia',
               expandedText : true,
               svgIcon: 'assets/svg/edit_contents.svg',
@@ -42,21 +40,99 @@ class MySubjectsPage extends StatelessWidget {
                 print("Pressed");
               }
             ),
-          ),
-            
 
-            Expanded(
+            Expanded( 
               child: SingleChildScrollView(
                 child: Padding(
-                  padding: EdgeInsets.symmetric( 
+                  padding: const EdgeInsets.symmetric( 
                     vertical: 20.0,
-                    horizontal: MediaQuery.of(context).size.width * 0.05,),
-                  
+                  ),
                   child: Column(
                     children: List.generate(30, (index) {
-                      return HomeSubject();
-                    })
-                  )
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                            elevation: 5.0,
+                            color: AppColors.ateneaWhite,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'Temas Selectos de Materia Genérica #${index + 1}',
+                                    style: AppTextStyles.builder(
+                                      color: AppColors.ateneaBlack,
+                                      size: FontSizes.body1,
+                                      weight: FontWeights.semibold
+                                    ),
+
+                                    
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start, 
+                                          children: [
+                                            Text(
+                                              'Autor',
+                                              style: 
+                                              AppTextStyles.builder(
+                                                color: AppColors.primaryColor,
+                                                size: FontSizes.body2,
+                                              )
+                                            ),
+
+                                            Text(
+                                              'Noguera Guzman',
+                                              style: 
+                                              AppTextStyles.builder(
+                                                color: AppColors.primaryColor,
+                                                size: FontSizes.body2,
+                                              )
+                                            ),
+                                          ],
+                                        )
+                                      ) ,
+                                      
+
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.end,
+                                          children: [
+                                            Text(
+                                              'Autor',
+                                              style: 
+                                              AppTextStyles.builder(
+                                                color: AppColors.primaryColor,
+                                                size: FontSizes.body2,
+                                              )
+                                            ),
+
+                                            Text(
+                                              'Noguera Guzman',
+                                              style: 
+                                              AppTextStyles.builder(
+                                                color: AppColors.primaryColor,
+                                                size: FontSizes.body2,
+                                              )
+                                            ),
+                                          ],
+                                        ) 
+                                      )
+                                    ] 
+                                  ), 
+                                ],
+                              )
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                  ))
                 )
               )
             )
