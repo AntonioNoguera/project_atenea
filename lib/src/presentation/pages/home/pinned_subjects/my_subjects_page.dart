841c8b6e6b6165ce17feeae1851810459b1b277d
Michael Guzmán <116075308+AntonioNoguera@users.noreply.github.com>
2024-11-19 16:15:42 -0600

    Feat [ Starting Def Empty Messages ] -> the main idea is to have a empty fallback

--- lib/src/presentation/pages/home/pinned_subjects/my_subjects_page.dart
+++ lib/src/presentation/pages/home/pinned_subjects/my_subjects_page.dart
@@ -1,71 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:proyect_atenea/src/presentation/pages/home/content_management/subject/subject_item_row.dart';
-import 'package:proyect_atenea/src/presentation/values/app_theme.dart'; 
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:proyect_atenea/src/presentation/values/app_theme.dart';
 import 'package:proyect_atenea/src/presentation/widgets/atenea_button_v2.dart';
 import 'package:proyect_atenea/src/presentation/widgets/atenea_scaffold.dart';
 
 class MySubjectsPage extends StatelessWidget {
   const MySubjectsPage({super.key});
- 
+
   @override
-  Widget build(BuildContext context) {     
-    return AteneaScaffold( 
-      body : Padding(
+  Widget build(BuildContext context) {
+    return AteneaScaffold(
+      body: Padding(
         padding: const EdgeInsets.symmetric(
-          vertical: 50.0
-          ),
-        
-        child: Column( 
+          vertical: 50.0,
+        ),
+        child: Column(
           children: [
             Text(
-                'Mis Materias',
-                style: AppTextStyles.builder(
-                      color: AppColors.primaryColor, 
-                      size: FontSizes.h2, 
-                      weight: FontWeights.semibold
-                    )
-              ),
-
-            const SizedBox(height: 20.0,),
-
-            Padding(padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.05,),
-              child: AteneaButtonV2(
-                text : 'Añadir Nueva Materia',
-                xpndText : true,
-
-                svgIcon : SvgButtonStyle(
-                  svgPath: 'assets/svg/add.svg' 
-                ), 
-                
-                onPressed: () {
-                  print('Pressed');
-                }
+              'Mis Materias',
+              style: AppTextStyles.builder(
+                color: AppColors.primaryColor,
+                size: FontSizes.h2,
+                weight: FontWeights.semibold,
               ),
             ),
-            
-
+            const SizedBox(height: 20.0),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.05,
+              ),
+              child: AteneaButtonV2(
+                text: 'Añadir Nueva Materia',
+                xpndText: true,
+                svgIcon: SvgButtonStyle(
+                  svgPath: 'assets/svg/add.svg',
+                ),
+                onPressed: () {
+                  print('Pressed');
+                },
+              ),
+            ),
             Expanded(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: EdgeInsets.symmetric( 
-                    vertical: 20.0,
-                    horizontal: MediaQuery.of(context).size.width * 0.05,),
-                  
-                  child: const Column(
-                    /*
-                    Todo, get Saved Subjects
-                    
-                    children: List.generate(30, (index) {
-                      return const HomeSubject();
-                    })
-                    */
-                  )
-                )
-              )
-            )
-          ] 
-        )
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/svg/crossed_folder.svg',
+                      height: 50,
+                      width: 50,
+                      color: AppColors.grayColor.withOpacity(0.9),
+                    ),
+                    const SizedBox(height: 10.0),
+                    Text(
+                      'No tienes materias guardadas',
+                      style: AppTextStyles.builder(
+                        color: AppColors.grayColor.withOpacity(0.9),
+                        weight: FontWeights.regular,
+                        size: FontSizes.h5,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
