9aa0d522342a6d60baa827b93a8d7df8d20e4955
Michael Guzmán <116075308+AntonioNoguera@users.noreply.github.com>
2024-11-29 23:59:27 -0600

    Feat [Create Academy] -> New functions that actually creates the new academyh

--- lib/src/presentation/pages/home/content_management/academies/create/academy_create_new_page.dart
+++ lib/src/presentation/pages/home/content_management/academies/create/academy_create_new_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
 import 'package:proyect_atenea/src/domain/entities/department_entity.dart';
-import 'package:proyect_atenea/src/domain/entities/shared/atomic_permission_entity.dart';
 import 'package:proyect_atenea/src/domain/entities/shared/enum_fixed_values.dart';
-import 'package:proyect_atenea/src/domain/entities/user_entity.dart';
-import 'package:proyect_atenea/src/presentation/pages/home/content_management/academies/create/widget/academy_contributor_row.dart';
+import 'package:proyect_atenea/src/domain/entities/academy_entity.dart';
+import 'package:proyect_atenea/src/presentation/pages/home/content_management/widgets/atenea_contributor_local_wokspace.dart';
+import 'package:proyect_atenea/src/presentation/providers/remote_providers/academy_provider.dart';
+import 'package:proyect_atenea/src/presentation/providers/remote_providers/session_provider.dart';
 
-import 'package:proyect_atenea/src/presentation/pages/home/content_management/academies/create/widget/add_contributor_dialog.dart';
-import 'package:proyect_atenea/src/presentation/pages/home/content_management/academies/create/widget/modify_contributor_dialog.dart';
- 
-import 'package:proyect_atenea/src/presentation/values/app_theme.dart'; 
+import 'package:proyect_atenea/src/presentation/values/app_theme.dart';
 import 'package:proyect_atenea/src/presentation/widgets/atenea_button_v2.dart';
 import 'package:proyect_atenea/src/presentation/widgets/atenea_dialog.dart';
 import 'package:proyect_atenea/src/presentation/widgets/atenea_field.dart';
@@ -25,7 +25,8 @@ class AcademyCreateNewPage extends StatefulWidget {
 
 class _AcademyCreateNewPageState extends State<AcademyCreateNewPage> {
   final TextEditingController _academyNameController = TextEditingController();
-  List<Map<String, dynamic>> _contributors = []; // Lista local de contribuidores
+  final GlobalKey<AteneaContributorLocalWorkspaceState> _childKey =
+      GlobalKey<AteneaContributorLocalWorkspaceState>();
 
   @override
   void dispose() {
@@ -33,33 +34,13 @@ class _AcademyCreateNewPageState extends State<AcademyCreateNewPage> {
     super.dispose();
   }
 
-  /// Añadir un nuevo contribuidor
-  void _addContributor(UserEntity contributor, AtomicPermissionEntity permission) {
-    setState(() {
-      _contributors.add({
-        'contributor': contributor,
-        'permission': permission,
-      });
-    });
+  void _invokeChildSaveChanges() {
+    _childKey.currentState?.saveChanges(context);
   }
 
-  /// Modificar permisos de un contribuidor existente
-  void _modifyContributor(int index, AtomicPermissionEntity updatedPermission) {
-    setState(() {
-      _contributors[index]['permission'] = updatedPermission;
-    });
-  }
-
-  /// Eliminar un contribuidor de la lista local
-  void _removeContributor(int index) {
-    setState(() {
-      _contributors.removeAt(index);
-    });
-  }
-
-  /// Guardar la academia
-  void _saveAcademy() {
+  Future<void> _saveAcademy() async {
     final academyName = _academyNameController.text.trim();
+
     if (academyName.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('El nombre de la academia no puede estar vacío.')),
@@ -67,36 +48,42 @@ class _AcademyCreateNewPageState extends State<AcademyCreateNewPage> {
       return;
     }
 
-    print('Academia guardada: $academyName');
-    print('Contribuidores: ${_contributors.map((c) => c['contributor'].fullName).join(', ')}');
+    // Invoca al método del widget hijo para guardar cambios locales
+    _invokeChildSaveChanges();
 
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Academia guardada con éxito')),
-    );
-    Navigator.pop(context);
-  }
+    final parentDepartmentRef = FirebaseFirestore.instance
+        .collection('departments')
+        .doc(widget.parentDepartment.id);
 
-  /// Mostrar diálogo para modificar un contribuidor
-  void _showModifyContributorDialog(
-    int index,
-    UserEntity contributor,
-    AtomicPermissionEntity currentPermission,
-  ) {
-    showDialog(
-      context: context,
-      builder: (_) => ModifyContributorDialog(
-        entityUUID: 'temp_academy_id',
-        entityType: SystemEntitiesTypes.academy,
-        userDisplayed: contributor,
-        permissionEntity: currentPermission,
-        onPermissionUpdated: (updatedContributor, updatedPermission) {
-          _modifyContributor(index, updatedPermission);
-        },
-        onPermissionRemoved: (removedContributor) {
-          _removeContributor(index);
-        },
-      ),
+    // Obtiene la sesión actual para recuperar el `userId`
+    final sessionProvider = Provider.of<SessionProvider>(context, listen: false);
+    final session = await sessionProvider.getSession();
+
+    if (session == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('No se pudo obtener la sesión del usuario.')),
+      );
+      return;
+    }
+
+    final newAcademy = AcademyEntity(
+      name: academyName,
+      parentDepartment: parentDepartmentRef,
+      subjects: [], 
+      lastModificationContributor: session.userId,
     );
+
+    try {
+      await Provider.of<AcademyProvider>(context, listen: false).addAcademy(newAcademy);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Academia guardada con éxito')),
+      );
+      Navigator.pop(context);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al guardar la academia: $e')),
+      );
+    }
   }
 
   @override
@@ -142,7 +129,7 @@ class _AcademyCreateNewPageState extends State<AcademyCreateNewPage> {
                           style: AppTextStyles.builder(
                             color: AppColors.primaryColor,
                             size: FontSizes.body2,
-                            weight: FontWeights.semibold, // Aplicar negritas al nombre del departamento
+                            weight: FontWeights.semibold,
                           ),
                         ),
                       ],
@@ -156,72 +143,21 @@ class _AcademyCreateNewPageState extends State<AcademyCreateNewPage> {
                   ),
                   const SizedBox(height: 20.0),
                   Text(
-                    'Contribuidores',
+                    'Contribuidores con Permisos',
                     style: AppTextStyles.builder(
                       size: FontSizes.h5,
                       color: AppColors.primaryColor,
                     ),
                   ),
                   const SizedBox(height: 10.0),
-                  _contributors.isEmpty
-                      ? Text(
-                          'No hay ningun contribuidor para esta academia, intenta dar un de alta.',
-                          style: AppTextStyles.builder(
-                            size: FontSizes.body2,
-                            color: AppColors.grayColor,
-                          ),
-                          textAlign: TextAlign.center,
-                        )
-                      : Flexible(
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: _contributors.length,
-                            itemBuilder: (context, index) {
-                              final contributor = _contributors[index]['contributor'] as UserEntity;
-                              final permission = _contributors[index]['permission'] as AtomicPermissionEntity;
-                              return Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 5.0),
-                                child: AcademyContributorRow(
-                                  index: index,
-                                  contributorName: contributor.fullName,
-                                  permissionEntity: permission,
-                                  showDetail: () {
-                                    _showModifyContributorDialog(index, contributor, permission);
-                                  },
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                  const SizedBox(height: 20.0),
-                  AteneaButtonV2(
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (_) => AddContributorDialog(
-                          entityUUID: 'temp_academy_id',
-                          entityType: SystemEntitiesTypes.academy,
-                          onPermissionAdded: _addContributor,
-                        ),
-                      );
-                    },
-                    textStyle: AppTextStyles.builder(
-                      color: AppColors.primaryColor,
-                      size: FontSizes.h5,
-                      weight: FontWeights.light,
+                  Flexible(
+                    child: AteneaContributorLocalWorkspace(
+                      key: _childKey,
+                      entityUUID: 'temp_academy_id',
+                      entityType: SystemEntitiesTypes.academy,
                     ),
-                    svgIcon: SvgButtonStyle(
-                      svgPath: 'assets/svg/add_user.svg',
-                      svgDimentions: 25,
-                    ),
-                    btnStyles: const AteneaButtonStyles(
-                      backgroundColor: AppColors.ateneaWhite,
-                      textColor: AppColors.primaryColor,
-                      hasBorder: true,
-                    ),
-                    text: 'Añadir Contribuidor',
                   ),
-                  const SizedBox(height: 60.0),
+                  const SizedBox(height: 20.0),
                 ],
               ),
             ),
